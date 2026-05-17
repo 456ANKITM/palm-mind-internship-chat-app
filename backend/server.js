ef977188e356b2461674f0bcb108a70b62b8a56f
Ankit Mishra <am9491691@gmail.com>
2026-05-17 12:12:17 +0545

    SOMETHING

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(
   cors({
-    origin: "http://localhost:5173" || "https://palm-mind-internship-chat-ad2iawo9j.vercel.app/",
+    origin: process.env.ORIGIN || "http://localhost:5173",
     credentials: true,
   })
 );
