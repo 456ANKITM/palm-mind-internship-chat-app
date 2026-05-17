5856065bfd71dda1b794d588eaa47e76eb42f13a
Ankit Mishra <am9491691@gmail.com>
2026-05-17 11:40:52 +0545

    removed ORIGIN to track real issue

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(
   cors({
-    origin: process.env.ORIGIN || "http://localhost:5173" || "https://palm-mind-internship-chat-ad2iawo9j.vercel.app/",
+    origin: "http://localhost:5173" || "https://palm-mind-internship-chat-ad2iawo9j.vercel.app/",
     credentials: true,
   })
 );
