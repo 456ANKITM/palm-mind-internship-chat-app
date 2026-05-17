4d36488d8d70036e9c0d9fad6d83d72a20b7d6b2
Ankit Mishra <am9491691@gmail.com>
2026-05-17 12:30:20 +0545

    cookie enhanced

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -102,11 +102,13 @@ export const login = async (req, res) => {
     const userData = user.toObject();
     delete userData.password;
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+  res.cookie("token", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  path: "/",
+});
 
     return res.status(200).json({
       success: true,
